392d318f32070cca3fb2781e1f7ec065df5aa7bd
vanomasss <maslikivan74@gmail.com>
2026-07-29 23:24:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from main import main
 import asyncio
+import threading
 
 app = Flask(__name__)
 
@@ -9,10 +10,9 @@ def home():
     return "Бот работает!"
 
 if __name__ == "__main__":
-    # Запускаем бота в отдельном потоке, чтобы Flask отвечал на пинги Render
-    import threading
     def run_bot():
         asyncio.run(main())
     
-    threading.Thread(target=run_bot, daemon=True).start()
+    thread = threading.Thread(target=run_bot, daemon=True)
+    thread.start()
     app.run(host="0.0.0.0", port=10000)
