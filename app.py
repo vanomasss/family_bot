309586df79379fc9dce7bf0a7fbd460117d24ed4
vanomasss <maslikivan74@gmail.com>
2026-07-29 23:30:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
-from flask import Flask
 from main import main
 import asyncio
-import threading
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Бот работает!"
 
 if __name__ == "__main__":
-    def run_bot():
-        asyncio.run(main())
-    
-    thread = threading.Thread(target=run_bot, daemon=True)
-    thread.start()
-    app.run(host="0.0.0.0", port=10000)
+    asyncio.run(main())
